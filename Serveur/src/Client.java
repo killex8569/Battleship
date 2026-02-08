2b3fbf6ca94288e22e188e5850609dcaba434b7d
killex8569 <alexandreanette@gmail.com>
2026-02-08 19:23:56 +0100

    Feat: Contructeur

--- Serveur/src/Client.java
+++ Serveur/src/Client.java
@@ -1,7 +1,11 @@
 public class Client {
     private Inventaire inventaire;
     public String nomJoueur;
-
+    // Constructeur
+    public Client(Inventaire inventaire, String nomJoueur){
+    this.inventaire = inventaire;
+    this.nomJoueur = nomJoueur;
+    }
     // Getter
     public Inventaire getInventaire(){
         return inventaire;
